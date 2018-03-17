fab7380f0a701ceb8b293280e424ce15198d9027
Patrick Miles <patrick.miles@gmail.com>
2018-03-17 14:06:05 -0700

    updates

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,32 @@
-$('<h1>').text('GRUB QUEST!!!').appendTo('body')
+
+
+
+
+
+$("#address").on("click", function(event) {
+
+
+    event.preventDefault();
+
+
+var street = $("#street_id").val().trim();
+var city = $("#city_id").val().trim();
+var state = $("#state_id").val().trim();
+var zip = $("#zip_id").val().trim();
+
+console.log (street);
+console.log (city);
+console.log (state);
+console.log (zip);
+
+
+});
+
+
+
+
+
+
+
+//Yelp get categories. 
+
