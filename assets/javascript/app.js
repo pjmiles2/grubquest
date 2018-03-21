806bf63281031f93961051f1534ba241a4992951
Erik C Hurst <ehurst01@gmail.com>
2018-03-20 17:43:43 -0700

    fixed variable API call, converted meters to miles (need to refactor), made number fields numbers in html.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,21 +4,15 @@
 
 
 $("#address").on("click", function(event) {
+event.preventDefault();
 
 
-    event.preventDefault();
-
-
-var street = $("#street_id").val().trim();
-var city = $("#city_id").val().trim();
-var state = $("#state_id").val().trim();
-var zip = $("#zip_id").val().trim();
-
-console.log (street);
-console.log (city);
-console.log (state);
-console.log (zip);
-
+let street = $("#street1_id").val().trim();
+let city = $("#city_id").val().trim();
+let state = $("#state_id").val().trim();
+let zip = $("#zip_id").val().trim();
+let radius = $('#radius').val().trim()*1609.34;
+foursquareSearch("&categoryId=4bf58dd8d48988d163941735",zip,radius);
 
 });
 
@@ -36,13 +30,14 @@ function foursquareSearch (cat, loc, rad){
     const clientID = '&client_id=P4KB5LUTWWYFAH4WWCI0OAA4UVU3NC0LKIKFJABAAAZ5ZBV0';
     const clientSecret ='&client_secret=VPWEYY3QVF2CU10AKLACJPBIDYR4QIPG2PUUSBY30FZUITVJ';
     const version = '&v=20170801';
-    let location = '&near='+loc;
-    let radius = '&radius='+rad;
-    let categoryID = '&categoryId=4bf58dd8d48988d112941735'
+    let location = String('&near='+loc);
+    let radius = String('&radius='+rad);
+    let categoryID = String(cat);
     const limit = '&limit=10'
   
     $.ajax({
-        url: 'https://api.foursquare.com/v2/venues/search?&client_id=P4KB5LUTWWYFAH4WWCI0OAA4UVU3NC0LKIKFJABAAAZ5ZBV0&client_secret=VPWEYY3QVF2CU10AKLACJPBIDYR4QIPG2PUUSBY30FZUITVJ&v=20170801&categoryId=4bf58dd8d48988d112941735&near=phoenix,az',
+        url: apiString+clientID+clientSecret+version+location+radius+categoryID+limit,
+        // url: 'https://api.foursquare.com/v2/venues/search?&client_id=P4KB5LUTWWYFAH4WWCI0OAA4UVU3NC0LKIKFJABAAAZ5ZBV0&client_secret=VPWEYY3QVF2CU10AKLACJPBIDYR4QIPG2PUUSBY30FZUITVJ&v=20170801&categoryId=4bf58dd8d48988d112941735&near=85015',
         method:'GET'
     }).done((response)=>{
         //display 
@@ -50,7 +45,7 @@ function foursquareSearch (cat, loc, rad){
         $('<h1>').text(JSON.stringify(response)).appendTo('body')
     });
 }
-foursquareSearch("x");  
+// foursquareSearch("x");  
 
 
 //Yelp get categories. 
